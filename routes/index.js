ccc755cd7de9b6df478e94420c928caaf8e62cd0
Denis Rastrepin <rusBeliy161@yandex.ru>
2023-11-01 13:53:24 +0300

    исправление ошибок4

--- routes/index.js
+++ routes/index.js
@@ -3,13 +3,13 @@ const router = require('express').Router();
 const userRouter = require('./users');
 const cardRouter = require('./cards');
 
+
+router.use('/users', userRouter);
+router.use('/cards', cardRouter);
 router.use('*', (req, res) => {
   res.status(404).send({
     message: 'Переданы некорректные данные',
   });
 });
-
-router.use('/users', userRouter);
-router.use('/cards', cardRouter);
 
 module.exports = router;
